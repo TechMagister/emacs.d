78d13e00d91931c391033208bf320eead2b66eba
Arnaud Fernandés <arnaud.fernandes@tech-magister.com>
2017-01-11 10:13:16 +0100

    Update repos

---
+++ personal/crystal.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path "~/.emacs.d/personal/repos/emacs-cracker")
+(add-to-list 'load-path "~/.emacs.d/personal/repos/emacs-crystal-mode")
+
+(require 'crystal-mode)
+(require 'flycheck)
+(require 'cracker)
+(require 'flycheck-crystal)
