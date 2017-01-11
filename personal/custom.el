b1872f317adada343f8a09a6ec6d2c3afc9f1887
Arnaud Fernandés <arnaud.fernandes@tech-magister.com>
2017-01-11 10:36:26 +0100

    Add important files

---
+++ personal/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(crystal-align-to-stmt-keywords (quote (def)))
+ '(custom-safe-themes
+   (quote
+    ("9122dfb203945f6e84b0de66d11a97de6c9edf28b3b5db772472e4beccc6b3c5" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "2bed8550c6f0a5ce635373176d5f0e079fb4fb5919005bfa743c71b5eed29d81" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (slim-mode org-dashboard dockerfile-mode php-mode material-theme flatland-black-theme flatland-theme dashboard niflheim-theme markdown-mode yaml-mode web-mode scss-mode geiser yari inf-ruby ruby-tools company-anaconda anaconda-mode json-mode js2-mode gotest go-projectile go-eldoc company-go go-mode rainbow-mode elisp-slime-nav rainbow-delimiters mediawiki company helm-ag helm-descbinds helm-projectile helm smex ido-ubiquitous flx-ido zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ personal/preload/custom.el
@@ -0,0 +1,19 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(global-linum-mode t)
+
+(set-frame-font "Source Code Pro-12" nil t)
+
+;; Smooth scrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
+(setq create-lockfiles nil)
+
+;; Theme
+(setq prelude-theme 'material)
+
+;;; custom.el ends here
